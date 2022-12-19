dbf2f71431d41084dcc87a15bddf5a10f464ef1e
Georgi Iliev <georgi.iliev533@outlook.com>
2022-12-19 15:37:34 +0200

    Option 1 almost complete
    Refactored files, functions and other changes

--- ConsoleTools.h
+++ ConsoleTools.h
@@ -1,39 +1,86 @@
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#define VC_EXTRALEAN
+#include <iostream>
+#include "ConsoleAPIs.h"
 
-#include <Windows.h>
-
-#elif defined(__linux__)
-#include <sys/ioctl.h>
-#endif // Windows/Linux
-
-void getConsoleBoundaries(int &width, int &height) {
-#if defined(_WIN32)
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    width = (int) (csbi.srWindow.Right - csbi.srWindow.Left + 1);
-    height = (int) (csbi.srWindow.Bottom - csbi.srWindow.Top + 1);
-#elif defined(__linux__)
-    struct winsize w;
-    ioctl(fileno(stdout), TIOCGWINSZ, &w);
-    width = (int)(w.ws_col);
-    height = (int)(w.ws_row);
-#endif // Windows/Linux
+void printBlankLines(const int height, int &printedLines, const int delta) {
+    for(int i = 0; i < height - printedLines - 1 - delta; i++) {
+        std::cout << "\n";
+    }
 }
 
-void clearConsole() {
-#if defined(_WIN32)
-        system("cls");
-        //clrscr(); // including header file : conio.h
-#elif defined(__linux__)
-        system("clear");
-    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences
-#elif defined (__APPLE__)
-    system("clear");
-#endif
+void printLogo(const int width, int &printedLines) {
+
+    char**  logoParts = new char*[5];
+    logoParts[0] = new char[] {R"(   _____ __  _____        ___   ___________)"};
+    logoParts[1] = new char[] {R"(  / ___// / / /   |      |__ \ / ____/ ___/)"};
+    logoParts[2] = new char[] {R"(  \__ \/ /_/ / /| |________/ //___ \/ __ \)"};
+    logoParts[3] = new char[] {R"( ___/ / __  / ___ /_____/ __/____/ / /_/ /)"};
+    logoParts[4] = new char[] {R"(/____/_/ /_/_/  |_|    /____/_____/\____/)"};
+
+    for(int i = 0; i < 5; i++) {
+        // logo is 43 chars wide
+        for(int j = 0; j < (width - 43) / 2; j++) {
+            std::cout << " ";
+        }
+        std::cout << logoParts[i] << "\n";
+        printedLines++;
+    }
+
 }
 
-void printLogo() {
+void printHeader(const int width, int &printedLines) {
+    printLogo(width, printedLines);
+    std::cout << "\n\n"; printedLines += 2;
+}
+
+void printOptions(int &printedLines) {
+    std::cout << "1. Hash from console" << std::endl;
+    std::cout << "2. Hash from file" << std::endl;
+    std::cout << "3. Print hash from file" << std::endl;
+    std::cout << "0. Exit from this application" << std::endl;
+    printedLines += 4;
+}
+
+void printExitMessage() {
+    clearConsole();
+    std::cout << "Have a nice day!" << std::endl;
+}
+
+
+void printInitialScreen() {
+    clearConsole();
+    int width=0, height=0, printedLines = 0;
+    getConsoleBoundaries(width, height);
+
+    printHeader(width, printedLines);
+
+    printOptions(printedLines);
+    std::cout << "\n\n"; printedLines += 2;
+
+    printBlankLines(height, printedLines, 0);
+}
+
+void printOptionA() {
+    clearConsole();
+    int width=0, height=0, printedLines = 0;
+    getConsoleBoundaries(width, height);
+
+    printHeader(width, printedLines);
+
+    std::cout << "You have chosen: hashing from console input.";
+    std::cout << "\n\n"; printedLines += 2;
+    std::cout << "Instructions: Enter the text you want to hash. When you are ready press ENTER to go to the next line and press CTRL + Z and ENTER again to finish entering text.\n\n";
+
+//    printBlankLines(height, printedLines, 0);
+}
+
+void printOptionB() {
+
+}
+
+void printOptionC() {
+
+}
+
+void printDefaultOption() {
 
 }
--- main.cpp
+++ main.cpp
@@ -2,26 +2,67 @@
 #include "ConsoleTools.h"
 #include "SHA256.h"
 
+const int TEXT_LENGTH = 2000;
 
+void optionA() {
+    int length = 0;
+    char *text = new char[TEXT_LENGTH];
+    std::cin >> std::ws;
+    while(true) {
+        std::cin.getline(text, TEXT_LENGTH);
+        text[length-2] = '\0';
+        std::cout << "Result: ";
+        printSHA(text);
+        std::cout << "\n\n";
+        if(std::cin.eof()) {
+            break;
+        }
+    }
+    delete[] text;
+}
+
+void optionB() {
+
+    std::cout << "Option B";
+}
+
+void optionC() {
+
+    std::cout << "Option c";
+}
+
+void defaultOption() {
+
+    std::cout << "Option D";
+}
 
 int main() {
-    printSHA("hello world");
-//    int width=0, height=0;
-//    clearConsole();
-//    getConsoleBoundaries(width, height);
-//    std::cout << R"(
-//   _____ __  _____        ___   ___________
-//  / ___// / / /   |      |__ \ / ____/ ___/
-//  \__ \/ /_/ / /| |________/ //___ \/ __ \
-// ___/ / __  / ___ /_____/ __/____/ / /_/ /
-///____/_/ /_/_/  |_|    /____/_____/\____/
-//
-//)";
-////    std::cout << "width=" << width << ", height=" << height << std::endl;
-//    std::cout << "Press any key to exit!" << std::endl;
-//    std::cin.get();
-//    std::cout << "";
-//    clearConsole();
-//    std::cout << "Have a nice day!" << std::endl;
+    int choice = -1;
+    while(choice != 0) {
+        clearConsole();
+        printInitialScreen();
+        std::cin >> choice;
+        switch(choice) {
+            case 0: printExitMessage(); break;
+            case 1:
+                printOptionA();
+                optionA();
+            break;
+            case 2:
+                printOptionB();
+                optionB();
+            break;
+            case 3:
+                printOptionC();
+                optionC();
+            break;
+            default:
+                printDefaultOption();
+                defaultOption();
+            break;
+        }
+    }
+
     return 0;
 }
+
